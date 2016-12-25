e91d9fe13ad1bd5c174d8202041f9d28ab6f6e42
遇松 <wuanrin@hotmail.com>
2016-12-25 15:54:25 +0800

    Add JSON Support

--- src/main/java/me/anrin/controller/HomeController.java
+++ src/main/java/me/anrin/controller/HomeController.java
@@ -1,9 +1,13 @@
 package me.anrin.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 // 控制器
 @Controller
@@ -19,5 +23,14 @@ public class HomeController {
 	public String detail(Model model) { 
 		model.addAttribute("name", "Michael Jordan");
 		return "detail";
+	}
+
+	// 返回 josn 格式
+	@RequestMapping("/json")
+	@ResponseBody
+	public Map<String,Object> json() {
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("success", true);
+		return map;
 	}
 }
